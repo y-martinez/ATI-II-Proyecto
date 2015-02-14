128466fe3eb1d74c02b0908411c06ae93545e463
Ybrahin Martinez <ybrahimmartinez09@gmail.com>
2015-02-14 03:37:37 -0430

    Registrar el usuario bueno

--- spot_parking_app/app/controllers/users_controller.rb
+++ spot_parking_app/app/controllers/users_controller.rb
@@ -9,19 +9,35 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(user_params)
-		@user.date_creation = Time.now
-		@user.time_of_reservation = nil
-		if @user.save then
-			flash[:message] = "User Created"
-			redirect_to users_url
-		else
-			flash[:message] = "Error"
+		@user = User.where(email: params[:user][:email]).take rescue user = nil
+		if @user
+			flash[:message] = "Email already used"
 			render action: 'new'
+		else
+			puts user_params
+			@user = User.new(user_params)
+			@user.type_of_user = 'User'
+			@user.date_creation = Time.now
+			@user.time_of_reservation = nil
+			if @user.save then
+				flash[:message] = "User Created"
+				puts 'Error 1'
+				#redirect_to users_url Error al redireccionar
+				redirect_to root_path
+			else
+				flash[:message] = "Error"
+				puts 'Error 2'
+				render action: 'new' 
+			end
 		end
 	end
 
 	def delete
 		
 	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email, :password)
+		end
 end
